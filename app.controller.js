a88ae0b59832794e7053297df44bb99b09999747
jgilsosa <jgilsosa@makerelectronica.com.ar>
2022-05-16 20:14:25 -0300

    - MOD => Boton efecto hover leer más     - ADD => Linea entre titulo y sub titulo     - ADD => Efecto fadeIn fadeOut para distintos subtitulos     - MOD => Estrucura javascript, cambiada a ECMAScript5

--- app.controller.js
+++ app.controller.js
@@ -1,8 +1,46 @@
-angular.module("App", [])
-    .controller("mainController", mainController);
-    
-function mainController ($scope) {
+(function() {
+    'use-strict';
 
+angular.module("App", [])
+.directive('app', [() => {
+    return {
+        restrict: 'E',
+        templateUrl: 'index.html'
+    }
+}])
+
+.controller("mainController", mainController);
+    
+function mainController ($scope, $interval) {
+
+    var self = this;
+    
+    // --------------- Subtitlos --------------- // 
+    
+    self.subtitles = [
+        'Excelente calidad y precios accesibles',
+        'Home Spray',
+        'Textile Flavoring',
+        'Candles & Essencess',
+    ];
+
+    self.cont = 0;
+    self.subtitle = 'Bienvenidos a Karoma Home';
+    $interval(async () => {
+        if(self.cont == self.subtitles.length){ self.cont = 0; }
+        
+        let myQuery = $('#fondo-1 div h2');
+
+        myQuery.fadeOut(() => { 
+            myQuery.text(`${self.subtitles[self.cont]}`);
+            self.cont+=1;
+        }).fadeIn();
+
+    }, 4000);
+
+    // --------------- Fin Subtitlos --------------- // 
+    
+    // --------------- Imagenes --------------- // 
     $scope.dir = 'assets/img/in_use/';
     // Debe actualizarse cuando se agregan nuevas fotos que seran usadas en las tarjetas
     $scope.cards = [
@@ -37,8 +75,14 @@ function mainController ($scope) {
         },
 
     ];
-
+    // --------------- Fin Imagenes --------------- // 
+    
     $scope.velasyescencia = 'El proceso de selección de una pieza contenedora y su perfumees único, cada vela es vertida a mano. Con materias primas de excelente calidad';
     $scope.homespray = 'Aromatizador de ambiente/textil, esta formulado con un alto porcentaje de fragancia de perfumeria fina, que te va a asegurar un aroma intenso y duradero.';
     $scope.salesbanio = 'Para un baño refrescante y relajante, ';
-}
+    
+
+} // Fin Controller 
+
+})();
+
