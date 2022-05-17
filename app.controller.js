f24c0ce1d8d96ce26b3c6fe5592047b386b1e6f1
jgilsosa <jgilsosa@makerelectronica.com.ar>
2022-05-17 19:56:30 -0300

    NUEVA PAGINA     - DEL => Fotos, tarjetas, ahora solo 4     - ADD => Titulo para ese div

--- app.controller.js
+++ app.controller.js
@@ -44,11 +44,11 @@ function mainController ($scope, $interval) {
     $scope.dir = 'assets/img/in_use/';
     // Debe actualizarse cuando se agregan nuevas fotos que seran usadas en las tarjetas
     $scope.cards = [
-        {
-            file: 'velas.jfif', 
-            description: 'Una vela de soja con aroma lavanda y jabón liquido con fragancia a naranja', 
-            title: 'Vela y Difusor'
-        },
+        // {
+        //     file: 'velas.jfif', 
+        //     description: 'Una vela de soja con aroma lavanda y jabón liquido con fragancia a naranja', 
+        //     title: 'Vela y Difusor'
+        // },
         {
             file: 'a.jpeg',        
             description: 'Vela abierta con pabilo de madera de bajo consumo, optimo para disfrutar del aroma por más tiempo', 
@@ -57,11 +57,6 @@ function mainController ($scope, $interval) {
         {
             file: 'b-sm.jpeg',     
             description: 'Velas con protector de madera, para su protección luego de ser apagada, asi no se ensucia', 
-            title: 'Velas'
-        },
-        {
-            file: 'b.jpeg',        
-            description: '', 
             title: 'Velas'
         },
         {
